d17801d7e94706212b484144012d26435bc3636e
qodvilla <villa@qodbrains.com>
2022-03-18 09:08:02 +0200

    Set up and Test theme

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Basic } from "./src/features/products/components/product-info-card.component";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ThemeProvider theme={theme}>
+      <Basic />
       <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
