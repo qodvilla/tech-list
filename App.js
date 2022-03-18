336bdb8e6c6e7ddd6433838364e09c5ce2d7300c
qodvilla <villa@qodbrains.com>
2022-03-18 15:06:54 +0200

    Added a Bottom Tab Navigation

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
-import { StyleSheet} from "react-native";
+import { StyleSheet } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
-import {ProductsScreen} from "./src/features/products/screens/products-screen";
+import { ProductsScreen } from "./src/features/products/screens/products-screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+const Navigator = Tab.Navigator;
+const ScreenComponent = Tab.Screen;
+
+const TAB_ICON = {
+  Products: 'tv-outline',
+  Favourites: 'heart-outline',
+  Sell: 'cash-outline',
+  Profile: 'person-outline'
+}
+
+const createScreenOptions = ({route}) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ color, size }) => <Ionicons name={iconName} size={size} color={color} />,
+  };
+};
 
 import {
   useFonts as useTitilliumWebFont,
@@ -29,7 +51,16 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <ProductsScreen />
+      <NavigationContainer>
+        <Navigator
+          screenOptions={createScreenOptions}
+        >
+          <ScreenComponent name="Products" component={ProductsScreen} />
+          <ScreenComponent name="Favourites" component={ProductsScreen} />
+          <ScreenComponent name="Sell" component={ProductsScreen} />
+          <ScreenComponent name="Profile" component={ProductsScreen} />
+        </Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
